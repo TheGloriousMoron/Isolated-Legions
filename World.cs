1e24405af4983def555b6c8a18c6f4e8758ed6d0
TheGloriousMoron <128844334+TheGloriousMoron@users.noreply.github.com>
2024-01-15 10:11:22 +0100

    Update World.cs

--- World.cs
+++ World.cs
@@ -4,6 +4,13 @@ namespace SUPLLY
 {
 	internal class World
 	{
+		internal int[] worldSize;
 		internal int meterSize;
+		
+		internal WOrld( int[] ws, int ms )
+		{
+			this.worldSize = ws;
+			this.meterSize = ms;
+		}
 	}
 }
