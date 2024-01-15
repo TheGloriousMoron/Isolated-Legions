225befda98c6ac8de9b67dd8d41a096d49880aba
TheGloriousMoron <128844334+TheGloriousMoron@users.noreply.github.com>
2024-01-15 10:18:19 +0100

    Create GameObject.cs

---
+++ GameObject.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace SUPLLY
+{
+	internal class GameObject
+	{
+		internal int[] objPos;
+		internal string ID;
+
+		internal GameObject(  )
+		{
+			this.objPos
+		}
+	}
+
+	int
+
+	internal int[] SetPosition()
+	{
+		
+	}
+}
